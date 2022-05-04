41872b156fb166f3593f76ea4d1f11efd7c967ba
Rafał Chełstowski <chelstowskirafaldev@gmail.com>
2022-05-04 22:46:01 +0200

    vec normalization

--- src/main.c
+++ src/main.c
@@ -93,13 +93,17 @@ void update(void)
             transformed_vertices[j] = transformed_vertex;
         }
 
-        // culling
         vec3_t vector_a = transformed_vertices[0];
         vec3_t vector_b = transformed_vertices[1];
         vec3_t vector_c = transformed_vertices[2];
         vec3_t vector_ab = vec3_sub(vector_b, vector_a);
         vec3_t vector_ac = vec3_sub(vector_c, vector_a);
+        vec3_normalize(&vector_ab);
+        vec3_normalize(&vector_ac);
         vec3_t normal = vec3_cross(vector_ab, vector_ac);
+
+        vec3_normalize(&normal);
+
         vec3_t camera_ray = vec3_sub(camera_position, vector_a);
         float dot_normal_camera = vec3_dot(normal, camera_ray);
 
